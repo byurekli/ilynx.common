cb72aee0261a4734b02fb81dc573f7f4ced45012
ilynx <t0yk4t@gmail.com>
2011-07-01 10:20:37 +0000

    Added protobuf-net trunk code.

--- skeinfish/Properties/AssemblyInfo.cs
+++ skeinfish/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SkeinFish")]
-[assembly: AssemblyDescription("An implementation of Skein and Threefish in C#")]
+[assembly: AssemblyTitle("Skeinfish")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alberto Fajardo")]
-[assembly: AssemblyProduct("SkeinFish")]
-[assembly: AssemblyCopyright("Copyright © Alberto Fajardo 2010")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Skeinfish")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("b87fa5b4-0224-49f8-91bd-61409df8e9b8")]
+[assembly: Guid("109985cc-42df-4272-a7f0-8e9a24d1a41f")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.5.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
