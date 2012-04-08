b08f7a2a46e9baf84d491adca2939a36957e7550
ilynx <t0yk4t@gmail.com>
2012-04-08 19:01:38 +0000

    Excluded bin
    Loading time should be improved for Hasherer

--- Hasherer/App.xaml.cs
+++ Hasherer/App.xaml.cs
@@ -4,6 +4,8 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Windows;
+using System.IO;
+using System.Reflection;
 
 namespace Hasherer
 {
@@ -12,5 +14,11 @@ namespace Hasherer
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            IEnumerable<string> loadedAssemblies = AppDomain.CurrentDomain.GetAssemblies().Select<Assembly, string>(a => a.Location);
+            foreach (string referencedButNotLoaded in Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory, "*.dll").Where(s => !loadedAssemblies.Contains(s, StringComparer.InvariantCultureIgnoreCase)))
+                AppDomain.CurrentDomain.Load(AssemblyName.GetAssemblyName(referencedButNotLoaded));
+        }
     }
 }
