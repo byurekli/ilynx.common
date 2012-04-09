b8b04a6430dfbbdc2d2ce6710cd1003036b8ae71
ilynx <t0yk4t@gmail.com>
2012-04-09 15:06:51 +0000

    Added ClosableTabItem Merged "FlatTheme.xaml" with WPFTools/Themes/Generic.xaml

---
+++ T0yK4T.WPFTools/ClosableTabItem.cs
@@ -0,0 +1,36 @@
+﻿
+
+using System.Windows.Controls;
+using System.Windows;
+using System;
+namespace T0yK4T.WPFTools
+{
+    public class ClosableTabItem : TabItem
+    {
+        public static readonly RoutedEvent CloseTabEvent = EventManager.RegisterRoutedEvent("CloseTab", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ClosableTabItem));
+
+        public event RoutedEventHandler CloseTab
+        {
+            add { base.AddHandler(CloseTabEvent, value); }
+            remove { base.RemoveHandler(CloseTabEvent, value); }
+        }
+
+        static ClosableTabItem()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(ClosableTabItem), new FrameworkPropertyMetadata(typeof(ClosableTabItem)));
+        }
+
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            Button closeButton = base.Template.FindName("PART_Close", this) as Button;
+            if (closeButton != null)
+                closeButton.Click += new RoutedEventHandler(closeButton_Click);
+        }
+
+        void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.RaiseEvent(new RoutedEventArgs(CloseTabEvent, this));
+        }
+    }
+}
